808c8543ad2bd407bcfd7983717144f502a310a3
JN <joyng808@gmail.com>
2025-07-09 23:28:16 +0800

    [JN]-Update

--- index.ts
+++ index.ts
@@ -4,3 +4,44 @@ console.log("hello");
 
 
 console.log("hellrssssddd");
+
+//----------------
+//------ 5 -------
+//----------------
+
+const students = [
+    {
+        name:"John",
+        score:70,
+    },
+    {
+        nanme:"Mark",
+        score:80,
+
+    }
+]; 
+
+
+
+for (const student of students){
+    console.log("Name:", student.name);
+    console.log("Score:",student.score);
+}
+
+
+//----------------
+//------ 6 -------
+//----------------
+
+function addNumber(a:number, b:number){
+    return a +b;
+}
+
+console.log(addNumber(2,3));
+
+//console.log(addNumber("text",3));
+
+
+
+
+
