843dfca663bc9ebe46aa5a02c5b61cd772b07409
JN <joyng808@gmail.com>
2025-07-21 23:31:21 +0800

    [JN]-Update Any

--- Section3_ExploringTypeScriptSystem/17_Any.ts
+++ Section3_ExploringTypeScriptSystem/17_Any.ts
@@ -1,3 +1,13 @@
-let firstname: any ="Mark"
+let firstname2: any ="Mark"
+firstname2 = 123;
+firstname2 = [];
+
+let firstname3: string ="ark"
+
+firstname3 = 123;
+firstname3 = [];
 
 
+function returnParam(param){
+    return param;
+}
