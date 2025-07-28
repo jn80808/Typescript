e76204b03c2bd3bc907fb9851c710d00377782c4
JN <joyng808@gmail.com>
2025-07-29 00:00:54 +0800

    [JN]-Update

--- Section4_ObjectTypes/29.TypeAliasForObject.ts
+++ Section4_ObjectTypes/29.TypeAliasForObject.ts
@@ -5,13 +5,40 @@ let post ={
 
 };
 
+//added strict line annotation 
+let post11: {
+    title: string;
+    content: string;
+    date: Date;
+} = {
+    title: "This is a blog post",
+    content: "Content of the post",
+    date: new Date(),
+};
 
 
+// Different Approach 
+
+type Post ={
+    title: string;
+    content: string;
+    date: Date;
+}
 
 
+let post2 : Post ={
+    title: "This is a blog post",
+    content: "Content of the post",
+    date: new Date(),
+    
+};
 
-
-
-
+//adding new properties that not include in the type Post 
+let post3 : Post ={
+    title: "This is a blog post",
+    content: "Content of the post",
+    date: new Date(),
+    category
+};
 
 
