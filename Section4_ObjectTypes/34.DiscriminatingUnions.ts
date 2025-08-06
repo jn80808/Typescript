70e9fef172d233d85ae524fc33def83fb2e8668c
JN <joyng808@gmail.com>
2025-08-06 18:09:17 +0800

    [JN]-Update

--- Section4_ObjectTypes/34.DiscriminatingUnions.ts
+++ Section4_ObjectTypes/34.DiscriminatingUnions.ts
@@ -4,16 +4,36 @@ type NetworkLoadingState = {
 
 
 type NetworkFieldLoadState = {
-    state : "Field";
+    state : "Failed";
     code: number; 
 };
 
 
 type NetworkSuccessSate = {
     state : "Success";
+    response :{
+        title : string;
+        duration: number;
+        summary: string; 
+    }
 }; 
 
+//Union of NetworkState 
+type NetworkState = NetworkLoadingState | NetworkFieldLoadState | NetworkSuccessSate ; 
 
+//return base on state application 
+function logger(state:NetworkState){
 
+    switch (state.state){
+        case "Loading":
+            return "Loading..";
+        case "Failed":
+            return `Error ${state.code}`
+        case "Success":
+            return `Downloading ${state.response.title}`;
+    }
+
+    
+}
 
 
