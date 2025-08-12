14cd2d1f92940bcf9bc486146c7f8d84eea1cd2a
JN <joyng808@gmail.com>
2025-08-13 00:03:24 +0800

    [JN]-Update

--- Section5_ArrayAndEnums.ts/41.ReadOnlyArraysandTuples.ts
+++ Section5_ArrayAndEnums.ts/41.ReadOnlyArraysandTuples.ts
@@ -7,8 +7,23 @@ let number1 : readonly number[] =  [1,2,3];
 
 
 
+//Regular Tuple 
+
+type RegularTuple = [string, string, number];
+
+// Tuple with ReadOnly 
+
+type ReadOnlyTuple = readonly [string, string, number];
 
 
+let person8 : ReadOnlyTuple = ["Joy", "Ng", 27];
+
+person8[0] = "dd"
+
+
+//alternative way to declare a readonly array 
+
+type a = Readonly<string[]>;
 
 
 
