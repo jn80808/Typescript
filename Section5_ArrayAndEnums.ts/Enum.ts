5b5d4e90766fb6b91b39184a74bdc62d0df6c9df
JN <joyng808@gmail.com>
2025-08-16 20:26:38 +0800

    [JN]-Added

---
+++ Section5_ArrayAndEnums.ts/Enum.ts
@@ -0,0 +1,21 @@
+const STATUS_LOADING ="loading";
+
+const STATUS_STOPPED ="stopped";
+
+
+
+STATUS_LOADING ="stopped"; 
+
+//Declaring ENUMS 
+// SYTAX :
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+
+
+
