270ee9e2d29664ef53eeea0bb76205713b5abe64
JN <joyng808@gmail.com>
2025-09-26 22:02:00 +0800

    [JN]-Update

--- Section7_GenericsInTypeScripts/63.GenericFunction Declarations.ts
+++ Section7_GenericsInTypeScripts/63.GenericFunction Declarations.ts
@@ -1,1 +1,23 @@
-const getFirstElement () ={}
+type GetFirstElement = <T> (arr: T[]) => T;
+
+//Normal
+const getFirstElement = (arr) =>{
+    return arr[0]
+}
+
+//Applying the Generic Type GetFirstElement
+const getFirstElement1: GetFirstElement = (arr) => {
+    return arr[0]
+}
+
+const numbersArray = [1,223,22,3,,4,,]
+
+const stringArray = ["s","d","r","f"]
+
+let numberOutput = getFirstElement<number>(numbersArray);
+
+
+
+
+
+
