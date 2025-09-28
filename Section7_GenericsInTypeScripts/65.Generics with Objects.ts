93d23184d577445093b3c75c17ab9f723c036b1d
JN <joyng808@gmail.com>
2025-09-28 22:56:00 +0800

    [JN[-Update

---
+++ Section7_GenericsInTypeScripts/65.Generics with Objects.ts
@@ -0,0 +1,19 @@
+//static 
+let stringNumberPair ={
+    key: "age",
+    value: 30
+}
+
+
+let NumberArrayPair ={
+    key: 123,
+    value: ["a"]
+}
+
+//type generic key: value pair 
+type KeyValuePair<K,V> = {
+    key: K;
+    value: V;
+}
+
+
