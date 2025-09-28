e4a774b0785233f2817db8b6b6f4bcb6adb7c721
JN <joyng808@gmail.com>
2025-09-28 23:37:44 +0800

    [JN]-Update

--- Section7_GenericsInTypeScripts/65.Generics with Objects.ts
+++ Section7_GenericsInTypeScripts/65.Generics with Objects.ts
@@ -24,9 +24,30 @@ let stringNumberPair1: KeyValuePair<string,number> ={
 }
 
 
-let NumberArrayPair1 ={
+let NumberArrayPair1 :KeyValuePair<number,string[]> ={
     key: 123,
     value: ["a"]
 }
+
+
+type HasId ={
+    id: number;
+}
+
+function printId<T extends HasId>(obj:T){
+    console.log(obj.id);
+}
+
+
+const user = {
+    id: 1234,
+    name:"Alice"
+
+}
+
+
+
+
+
 
 
